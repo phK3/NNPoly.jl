238c001cc423075175244edfd01557e62d443e4b
phK3 <ufedz@student.kit.edu>
2022-08-02 20:45:50 +0200

    update project dependencies and includes

--- src/NNPoly.jl
+++ src/NNPoly.jl
@@ -1,6 +1,6 @@
 module NNPoly
 
-using LazySets, DynamicPolynomials, RecipesBase, DataStructures, NeuralVerification, Parameters
+using LazySets, DynamicPolynomials, RecipesBase, DataStructures, NeuralVerification, Parameters, LinearAlgebra
 const NV = NeuralVerification
 
 include("utils.jl")
@@ -8,6 +8,9 @@ include("sparse_polynomial.jl")
 include("polynomial_optimization.jl")
 include("approximations/chebyshev.jl")
 include("reachability/nn_poly_zono.jl")
+include("reachability/poly_interval.jl")
+include("reachability/nn_poly_symb.jl")
+
 
 
 
