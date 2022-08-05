ee218f29240e4c6d9de35c8c78cc494c0bd2198e
phK3 <ufedz@student.kit.edu>
2022-08-05 14:01:57 +0200

    include symbolic truncation files to NNPoly tool

--- src/NNPoly.jl
+++ src/NNPoly.jl
@@ -7,6 +7,12 @@ include("utils.jl")
 include("sparse_polynomial.jl")
 include("polynomial_optimization.jl")
 include("approximations/chebyshev.jl")
+
+include("symbolic_truncation/max_min_relaxations.jl")
+include("symbolic_truncation/univariate_monomial_relaxations.jl")
+include("symbolic_truncation/monomial_relaxation.jl")
+include("symbolic_truncation/truncation.jl")
+
 include("reachability/nn_poly_zono.jl")
 include("reachability/poly_interval.jl")
 include("reachability/nn_poly_symb.jl")
