4026a4201b8d45839c7bc0a16eb7ec4b16f61826
phK3 <ufedz@student.kit.edu>
2022-09-09 13:21:18 +0200

    implement parallel relaxations for max and min

--- src/symbolic_truncation/max_min_relaxations.jl
+++ src/symbolic_truncation/max_min_relaxations.jl
@@ -49,10 +49,43 @@ args:
     y - SparsePolynomial
 
 kwargs:
-    printing - (bool) whether to print obtained bounds on y - x 
+    printing - (bool) whether to print obtained bounds on y - x
     max_steps - (Integer) maximum number of steps in BaB procedure
     optimality_gap - (Float64) optimality gap for early stopping of BaB procedure
 """
 function relax_min_lower(x, y; printing=false, max_steps=30, optimality_gap=1e-4)
     return negate(relax_max_upper(negate(x), negate(y), printing=printing, max_steps=max_steps, optimality_gap=optimality_gap))
+end
+
+
+"""
+Creates parallel symbolic relaxation of [min(x,y), max(x, y)] where x,y can be any SparsePolynomials.
+
+We first compute μ(x, y) = 1/2*(x + y) and calculate bounds for μ(x, y) - x
+(which handily also translate into bounds for μ(x, y) - y) and shift μ(x,y) down
+and up by the corresponding bounds to obtain a lower and an upper relaxation that
+are parallel to each other.
+
+args:
+    x - SparsePolynomial
+    y - SparsePolynomial
+
+kwargs:
+    printing - (bool) whether to print obtained bounds on y - x
+    max_steps - (Integer) maximum number of steps in BaB procedure
+    optimality_gap - (Float64) optimality gap for early stopping of BaB procedure
+"""
+function relax_min_max_parallel(x, y; printing=false, max_steps=30, optimality_gap=1e-4)
+    p_xy = stack_polys([x, y])
+    μ = linear_map([0.5 0.5], p_xy)
+    p_diff = linear_map([-0.5 0.5], p_xy)
+
+    u =  max_in_dir_bab( [1], p_diff, printing=false, max_steps=max_steps, optimality_gap=optimality_gap)
+    l = -max_in_dir_bab([-1], p_diff, printing=false, max_steps=max_steps, optimality_gap=optimality_gap)
+    printing && println("bounds: ", [l, u])
+
+    l_relax = translate(μ, [min(l, -u)])
+    u_relax = translate(μ, [max(-l, u)])
+
+    return l_relax, u_relax
 end
