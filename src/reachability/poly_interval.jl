658dc9a6985028fb5cbc7e175a0b408e579a80e1
phK3 <ufedz@student.kit.edu>
2022-09-15 15:43:36 +0200

    fix issue that we pretended to truncate constant terms

--- src/reachability/poly_interval.jl
+++ src/reachability/poly_interval.jl
@@ -47,39 +47,46 @@ end
 Truncate the n_gens smallest generators in the sparse polynomial by overapproximating
 them with intervals.
 """
-function truncate_generators(sp::SparsePolynomial, n_gens::Integer)
+function truncate_generators_non_const(sp::SparsePolynomial, n_gens::Integer)
     n_gens <= 0 && return sp, sp
 
     n, m = size(sp.G)
     l2s = vec(sum(sp.G .^2, dims=1))
-    idxs = sortperm(l2s)[1:n_gens]
+    idxs = sortperm(l2s)
     all_idxs = 1:m
-    remaining_idxs = setdiff(all_idxs, idxs)
 
+    truncated = 0
+    truncated_idxs = zeros(n_gens)
     ub = zeros(n)
     lb = zeros(n)
-    for i in idxs
-        ei = sp.E[:,i]
 
-        #println("truncate: ", ei)
+    i = 0
+    while truncated < n_gens
+        i += 1
+        idx = idxs[i]
+        ei = sp.E[:,idx]
 
         if sum(ei) == 0
-            # it really doesn't make sense to truncate the center !?!
-            ub .+= sp.G[:,i]
-            lb .+= sp.G[:,i]
+            # can't truncate constants
+            continue
         elseif all(iseven.(ei))
             # if negative generator, we can add 0, as even exponent can never be negative
-            ub .+= max.(0, sp.G[:,i])
+            ub .+= max.(0, sp.G[:,idx])
             # if positive generator, we can add 0
-            lb .+= min.(0, sp.G[:,i])
+            lb .+= min.(0, sp.G[:,idx])
         else
-            ub .+= abs.(sp.G[:,i])
-            lb .-= abs.(sp.G[:,i])
+            ub .+= abs.(sp.G[:,idx])
+            lb .-= abs.(sp.G[:,idx])
         end
+
+        truncated += 1
+        truncated_idxs[truncated] = idx
     end
 
     #@show maximum(ub)
     #@show minimum(lb)
+    
+    remaining_idxs = setdiff(all_idxs, truncated_idxs)
 
     G = sp.G[:, remaining_idxs]
     E = sp.E[:, remaining_idxs]
