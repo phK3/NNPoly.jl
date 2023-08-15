c63d1017643a4b7c1ff03351eef5ac364136f2da
phK3 <ufedz@student.kit.edu>
2023-08-15 19:55:35 +0200

    refactor common code

---
+++ src/reachability/utils.jl
@@ -0,0 +1,17 @@
+
+
+function forward_layer(solver, L::NV.LayerNegPosIdx, input, α)
+    ŝ = forward_linear(solver, L, input)
+    s = forward_act(solver, L, ŝ, α)
+    return s
+end
+
+
+function NV.forward_network(solver, net::NV.NetworkNegPosIdx, input, αs)
+    s = input
+    for (L, α) in zip(net.layers, αs)
+        s = forward_layer(solver, L, s, α)
+    end
+
+    return s
+end
