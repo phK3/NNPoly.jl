ca310a24f9386142b1665b252c5d4e1ffdbf196d
phK3 <ufedz@student.kit.edu>
2023-09-23 11:17:42 +0200

    Generalize forward_network, s.t. ambiguity between aCROWN's forward_network and the util forward_network is not present anymore

--- src/reachability/utils.jl
+++ src/reachability/utils.jl
@@ -1,13 +1,13 @@
 
 
-function forward_layer(solver, L::NV.LayerNegPosIdx, input, α)
+function forward_layer(solver, L::Union{NV.Layer,NV.LayerNegPosIdx}, input, α)
     ŝ = forward_linear(solver, L, input)
     s = forward_act(solver, L, ŝ, α)
     return s
 end
 
 
-function NV.forward_network(solver, net::NV.NetworkNegPosIdx, input, αs)
+function NV.forward_network(solver, net::Union{NV.Network,NV.NetworkNegPosIdx}, input, αs)
     s = input
     for (L, α) in zip(net.layers, αs)
         s = forward_layer(solver, L, s, α)
