9d48ba1d51104b0bf7e02daaec362fb176232f8d
phK3 <ufedz@student.kit.edu>
2024-01-30 12:12:23 +0100

    optimisation method for implicit parameters in model

--- src/optimisation_loop.jl
+++ src/optimisation_loop.jl
@@ -134,3 +134,59 @@ function optimise(f, opt, x₀; params=OptimisationParams())
 
     return (x_opt=x_best, y_hist=y_hist, t_hist=t_hist, g_hist=g_hist, d_hist=d_hist, csims=csims)
 end
+
+
+"""
+Gradient-based optimisation procedure, when parameters are implicitly contained in the model.
+
+args:
+    f - function to optimise (depends on parameters in the model)
+    model - a Flux Chain with optimisable parameters
+    opt - optimiser to use (all Optimisers.jl are supported)
+"""
+function optimise(f, model::Chain, opt; params=OptimisationParams())
+    # TODO: is there an abstract type for all Flux models, not just Chain?
+    t_start = time()
+    
+    state_tree = Optimisers.setup(opt, model)
+
+    # x_best = x  # how can we get the best params?
+    y_best = Inf
+    steps_no_improvement = 0
+    y_hist = Float64[]
+    t_hist = Float64[]
+
+    for i in 1:params.n_steps
+        y, ∇model = withgradient(model) do m
+            f(m)
+        end
+
+        if y < y_best
+            y_best = y
+            steps_no_improvement = 0
+        else
+            steps_no_improvement += 1
+        end
+
+        t_now = time()
+        elapsed_time = t_now - t_start
+        params.save_times && push!(t_hist, elapsed_time)
+        params.save_ys && push!(y_hist, y)
+        if steps_no_improvement > params.patience
+            println("No improvement over the last ", params.patience, " iterations. Stopping early.")
+            break
+        elseif elapsed_time >= params.timeout
+            println("Timeout reached (", elapsed_time, " elapsed)")
+            break
+        end
+
+        if i % params.print_freq == 0
+            println(i, ": ", y)
+        end
+
+        Optimisers.update!(state_tree, model, ∇model[1])
+    end
+
+    # don't really need to return params here since they should be modified in-place in the model
+    return (y_hist=y_hist, t_hist=t_hist)
+end
