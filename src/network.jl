233d0e12a6b45cfae2e115bec8e98fc0ee5f7692
phK3 <ufedz@student.kit.edu>
2024-01-31 11:39:04 +0100

    add params for polynomial relaxation

--- src/network.jl
+++ src/network.jl
@@ -34,16 +34,27 @@ args:
 
 kwargs:
     dtype - element type of the weights and biases (defaults to Float64)
+    degree - a vector of relaxation degrees for every layer (except for the last one) or a 
+                single scalar (then all layers have params for that relaxation degree)
 
 returns:
     Flux.Chain of the layers with params for trainable activations
 """
-function onnx2CROWNNetwork(onnx_file; dtype=Float64)
+function onnx2CROWNNetwork(onnx_file; dtype=Float64, degree=1)
     ws, bs = load_network(onnx_file, dtype=dtype)
+    # is there a better way to expand a scalar to an array?
+    degrees = typeof(degree) <: Number ? [degree for w in ws[1:end-1]] : degree
 
     layers = []
-    for (W, b) in zip(ws[1:end-1], bs[1:end-1])
-        push!(layers, CROWNLayer(W, b, NV.ReLU(), similar(b)))
+    for (W, b, d) in zip(ws[1:end-1], bs[1:end-1], degrees)
+        if d == 1
+            α = similar(b)
+        else
+            # n_neurons × degree × 2 (params for lower and upper bound)
+            α = similar(b, length(b), d, 2)
+        end
+
+        push!(layers, CROWNLayer(W, b, NV.ReLU(), α))
     end
 
     push!(layers, CROWNLayer(ws[end], bs[end], NV.Id(), similar(bs[end], 0)))
