d8092e56b2ffd51897c7a929cb4c9b5362de0db0
phK3 <ufedz@student.kit.edu>
2024-01-31 11:40:13 +0100

    bounds computation with DiffPolyInterval domain

--- src/reachability/diff_poly_interval.jl
+++ src/reachability/diff_poly_interval.jl
@@ -20,3 +20,29 @@ function DiffPolyInterval(net, input_set)
     s = init_poly_interval(input_set)
     return DiffPolyInterval(s, lbs, ubs)
 end
+
+
+function DiffPolyInterval(net::Chain, input_set)
+    lbs = [similar(L.bias) for L in net.layers]
+    ubs = [similar(L.bias) for L in net.layers]
+
+    for i in eachindex(lbs)
+        # is there any way to directly set them to ±Inf while also getting  the possibly gpu type right?
+        lbs[i] .= -Inf
+        ubs[i] .= Inf
+    end
+
+    s = init_poly_interval(input_set)
+    return DiffPolyInterval(s, lbs, ubs)
+end
+
+
+"""
+Calculates concrete bounds for A*s + b for DiffPolyInterval s with common generators.
+"""
+function bounds(A::AbstractMatrix, b::AbstractVector, s::DiffPolyInterval)
+    L, U = interval_map_common(min.(0, A), max.(0, A), s.poly_interval.Low, s.poly_interval.Up, b)
+    ll, lu = bounds(L)
+    ul, uu = bounds(U)
+    return ll, uu
+end
