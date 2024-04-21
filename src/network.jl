de71688af062296c1613f002ec4d554fdbd25297
phK3 <ufedz@student.kit.edu>
2024-04-21 10:58:45 +0200

    make CROWNLayers executable for testing

--- src/network.jl
+++ src/network.jl
@@ -16,6 +16,11 @@ function (m::CROWNLayer{NV.ReLU, MN, BN, AN})(x) where {MN,BN,AN}
 end
 
 
+function (m::CROWNLayer{NV.Id, MN, BN, AN})(x) where {MN,BN,AN}
+    return m.weights * x .+ m.bias
+end
+
+
 # make params available for flux transformations e.g. |> gpu or |> f32
 Flux.@functor CROWNLayer
 
@@ -36,14 +41,21 @@ kwargs:
     dtype - element type of the weights and biases (defaults to Float64)
     degree - a vector of relaxation degrees for every layer (except for the last one) or a 
                 single scalar (then all layers have params for that relaxation degree)
+    first_layer_degree - convenience argument to quickly specify degree for first layer 
+                            (defaults to -1, s.t. no special case for first layer)
 
 returns:
     Flux.Chain of the layers with params for trainable activations
 """
-function onnx2CROWNNetwork(onnx_file; dtype=Float64, degree=1)
+function onnx2CROWNNetwork(onnx_file; dtype=Float64, degree=1, first_layer_degree=-1)
     ws, bs = load_network(onnx_file, dtype=dtype)
     # is there a better way to expand a scalar to an array?
-    degrees = typeof(degree) <: Number ? [degree for w in ws[1:end-1]] : degree
+    if first_layer_degree == -1
+        degrees = typeof(degree) <: Number ? [degree for w in ws[1:end-1]] : degree
+    else
+        @assert typeof(degree) <: Number "Setting first_layer_degree and a non-number arg for degree is not supported!"
+        degrees = [[first_layer_degree]; [degree for w in ws[2:end-1]]]
+    end
 
     layers = []
     for (W, b, d) in zip(ws[1:end-1], bs[1:end-1], degrees)
