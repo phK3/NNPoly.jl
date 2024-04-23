72b9c0841c7aa8af4a746f10a64ea00b7f168ddc
phK3 <ufedz@student.kit.edu>
2024-04-23 16:10:04 +0200

    time single pass added

--- src/vnnlib.jl
+++ src/vnnlib.jl
@@ -94,8 +94,8 @@ returns:
     all_steps - number of steps performed by verifier
     result - (String) SAT, UNSAT or inconclusive
 """
-function verify_vnnlib(solver, dir, params::OptimisationParams; logfile=nothing, max_properties=Inf, only_pattern=nothing, 
-                        save_history=false, save_times=false, force_gc=false, start_idx=1, stop_idx=nothing)
+function verify_vnnlib(solver, dir, params::OptimisationParams; logfile=nothing, max_properties=Inf, only_pattern=nothing,
+    save_history=false, save_times=false, force_gc=false, start_idx=1, stop_idx=nothing)
     f = CSV.File(string(dir, "/instances.csv"), header=false)
 
     # need y history to get access to final loss values
@@ -208,11 +208,127 @@ end
 
 
 function verify_vnnlib(solver, dir; logfile=nothing, max_properties=Inf, print_freq=50, n_steps=5000,
-    only_pattern=nothing, save_history=false, save_times=false, timeout=60., force_gc=false, start_idx=1,
+    only_pattern=nothing, save_history=false, save_times=false, timeout=60.0, force_gc=false, start_idx=1,
     stop_idx=nothing)
     params = OptimisationParams(n_steps=n_steps, timeout=timeout, print_freq=print_freq)
     save_history && (params.save_ys = true)
     save_times && (params.save_times = true)
-    return verify_vnnlib(solver, dir, params, logfile=logfile, max_properties=max_properties, only_pattern=only_pattern, 
-                        save_history=save_history, save_times=save_times, force_gc=force_gc, start_idx=start_idx, stop_idx=stop_idx)
+    return verify_vnnlib(solver, dir, params, logfile=logfile, max_properties=max_properties, only_pattern=only_pattern,
+        save_history=save_history, save_times=save_times, force_gc=force_gc, start_idx=start_idx, stop_idx=stop_idx)
+end
+
+
+
+function time_single_pass(solver, dir; logfile=nothing, n_rep=50, timeout=60., max_properties=Inf, only_pattern=nothing,
+    save_history=false, save_times=false, force_gc=false, start_idx=1, stop_idx=nothing)
+    f = CSV.File(string(dir, "/instances.csv"), header=false)
+
+    n = length(f)
+    stop_idx = isnothing(stop_idx) ? n : stop_idx
+    networks = String[]
+    properties = String[]
+    #results = String[]
+    y_starts = zeros(n)
+    ys = zeros(n)
+    y_hists = []
+    t_hists = []
+    #all_steps = zeros(Integer, n)
+    times = zeros(n)
+
+    old_netpath = nothing
+    net = nothing
+    net_npi = nothing
+    n_in = 0
+    n_out = 0
+
+    n_iters = Int64[]
+
+    cnt = 0
+
+    for (i, instance) in enumerate(f)
+        netpath, propertypath, time_limit = instance
+
+        if !isnothing(only_pattern) && !contains(netpath, only_pattern)
+            # just for now
+            continue
+        end
+        if (i < start_idx) || (i > stop_idx)
+            continue
+        end
+
+        if netpath != old_netpath
+            println("-- loading network ", netpath)
+            net = read_onnx_network(string(dir, "/", netpath), dtype=Float64)
+            old_netpath = netpath
+
+            net_npi = NV.NetworkNegPosIdx(net)
+
+            n_in = size(net.layers[1].weights, 2)
+            n_out = NV.n_nodes(net.layers[end])
+        end
+
+        rv = read_vnnlib_simple(string(dir, "/", propertypath), n_in, n_out)
+        specs = generate_specs(rv)
+        input_set, output_set = specs[1]  # for now just use one set (we only care about the input set anyways here)
+
+        println("\n### Property ", propertypath, " ###\n")
+        println("--- initial α ---")
+        s = initialize_symbolic_domain(solver, net_npi, input_set)
+        times_single = Float64[]
+        
+        
+        push!(n_iters, n_rep)
+        t_start = Base.time()
+        for i in 1:n_rep
+            t = @elapsed α0 = initialize_params(solver, net_npi, 2, s)
+            push!(times_single, t)
+
+            t_now = Base.time()
+            if t_now - t_start > timeout
+                n_iters[end] = i  # no full number of iterations
+                break
+            end
+        end
+
+        time = sum(times_single) / length(times_single)
+        println("\ttime = ", time)
+
+        push!(networks, netpath)
+        push!(properties, propertypath)
+        times[i] = time
+
+        cnt += 1
+        if cnt >= max_properties
+            break
+        end
+
+        # also backup, if sth goes wrong later on
+        if !isnothing(logfile)
+            save(logfile, "properties", properties, "times", times)
+        end
+
+        if force_gc
+            # force garbage collection 
+            GC.gc()
+        end
+    end
+
+    #=if !isnothing(logfile)
+    open(logfile, "w") do f
+    println(f, "network,property,result,time,steps")
+    [println(f, string(network, ", ", property, ", ", result, ", ", time, ", ", steps))
+    for (network, property, result, time, steps) in zip(networks, properties, results, times, all_steps)]
+    end
+    end=#
+
+    println("saving results ...")
+    if !isnothing(logfile)
+        save(logfile, "properties", properties, "times", times)
+    end
+
+    if save_history
+        return properties, times, n_iters
+    else
+        return properties, times, n_iters
+    end
 end
